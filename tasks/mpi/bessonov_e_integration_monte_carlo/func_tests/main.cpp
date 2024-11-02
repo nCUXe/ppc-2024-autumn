e474398f1f97e7969053b94b67ce41d91b09cea0
nCUXe <egorkabessonov1@gmail.com>
2024-11-02 10:40:53 +0300

    13th commit

--- tasks/mpi/bessonov_e_integration_monte_carlo/func_tests/main.cpp
+++ tasks/mpi/bessonov_e_integration_monte_carlo/func_tests/main.cpp
@@ -146,3 +146,50 @@ TEST(bessonov_e_integration_monte_carlo_mpi, EqualRangeTestMPI) {
     ASSERT_NEAR(reference_result[0], global_result[0], 2e-1);
   }
 }
+
+TEST(bessonov_e_integration_monte_carlo_mpi, RandomTestMPI) {
+  boost::mpi::communicator world;
+  std::vector<double> global_result(1, 0.0);
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(-10.0, 10.0);
+  double a = dis(gen);
+  double b = dis(gen);
+
+  if (a > b) std::swap(a, b);
+
+  if (a == b) b += 1.0;
+
+  int num_points = 100000;
+  if (world.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_points));
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(global_result.data()));
+  }
+
+  bessonov_e_integration_monte_carlo_mpi::TestMPITaskParallel testMpiTaskParallel(taskDataPar);
+  ASSERT_TRUE(testMpiTaskParallel.validation());
+  testMpiTaskParallel.pre_processing();
+  testMpiTaskParallel.run();
+  testMpiTaskParallel.post_processing();
+
+  if (world.rank() == 0) {
+    std::vector<double> reference_result(1, 0.0);
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&a));
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&b));
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(&num_points));
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(reference_result.data()));
+
+    bessonov_e_integration_monte_carlo_mpi::TestMPITaskSequential testMpiTaskSequential(taskDataSeq);
+    ASSERT_EQ(testMpiTaskSequential.validation(), true);
+    testMpiTaskSequential.pre_processing();
+    testMpiTaskSequential.run();
+    testMpiTaskSequential.post_processing();
+
+    ASSERT_NEAR(reference_result[0], global_result[0], 1e1);
+  }
+}
